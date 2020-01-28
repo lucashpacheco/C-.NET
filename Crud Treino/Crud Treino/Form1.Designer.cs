deaf2ae01247115e5ecb02d8b1e61630ff348098
Lucas Pacheco <lucashpmendonca@gmail.com>
2020-01-27 21:16:28 -0300

    CRUD - STUDIES - BUG PESQUISAR

---
+++ Crud Treino/Crud Treino/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Crud_Treino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtidpesquisa = new System.Windows.Forms.TextBox();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtapelido = new System.Windows.Forms.TextBox();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.txtlogin = new System.Windows.Forms.TextBox();
+            this.txtobs = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 184);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(636, 254);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(573, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "NOVO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ATUALIZAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(573, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ADCIONAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(573, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "REMOVER";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(575, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "PESQUISAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtidpesquisa
+            // 
+            this.txtidpesquisa.Location = new System.Drawing.Point(469, 9);
+            this.txtidpesquisa.Name = "txtidpesquisa";
+            this.txtidpesquisa.Size = new System.Drawing.Size(100, 20);
+            this.txtidpesquisa.TabIndex = 6;
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(118, 82);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(100, 20);
+            this.txtnome.TabIndex = 7;
+            // 
+            // txtapelido
+            // 
+            this.txtapelido.Location = new System.Drawing.Point(118, 118);
+            this.txtapelido.Name = "txtapelido";
+            this.txtapelido.Size = new System.Drawing.Size(100, 20);
+            this.txtapelido.TabIndex = 8;
+            this.txtapelido.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Location = new System.Drawing.Point(342, 118);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.Size = new System.Drawing.Size(100, 20);
+            this.txtsenha.TabIndex = 9;
+            // 
+            // txtlogin
+            // 
+            this.txtlogin.Location = new System.Drawing.Point(342, 82);
+            this.txtlogin.Name = "txtlogin";
+            this.txtlogin.Size = new System.Drawing.Size(100, 20);
+            this.txtlogin.TabIndex = 11;
+            this.txtlogin.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txtobs
+            // 
+            this.txtobs.Location = new System.Drawing.Point(118, 149);
+            this.txtobs.Name = "txtobs";
+            this.txtobs.Size = new System.Drawing.Size(324, 20);
+            this.txtobs.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "NOME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "APELIDO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(272, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "LOGIN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(272, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "SENHA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "OBS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(364, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "PESQUISA POR ID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "id";
+            // 
+            // txtid
+            // 
+            this.txtid.AutoSize = true;
+            this.txtid.Location = new System.Drawing.Point(118, 50);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(0, 13);
+            this.txtid.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtobs);
+            this.Controls.Add(this.txtlogin);
+            this.Controls.Add(this.txtsenha);
+            this.Controls.Add(this.txtapelido);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.txtidpesquisa);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtidpesquisa;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtapelido;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.TextBox txtlogin;
+        private System.Windows.Forms.TextBox txtobs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label txtid;
+    }
+}
+
---
+++ Crud Treino/Crud Treino/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Crud_Treino
+{
+    
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Crud;Data Source=DESKTOPW");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        int ID = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            ExibirDados();
+
+        }
+
+        private void ExibirDados()
+        {
+            try
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("SELECT * FROM list_users", con);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            LimparDados();
+            
+        }
+
+        private void LimparDados()
+        {
+            txtnome.Text = "";
+            txtapelido.Text = "";
+            txtlogin.Text = "";
+            txtsenha.Text = "";
+            txtobs.Text = "";
+            txtid.Text = "";
+            txtnome.Focus();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtnome.Text != "" && txtapelido.Text != "" && txtlogin.Text != "" && txtsenha.Text != "" && txtobs.Text != "")
+            {
+                try
+                {
+                    cmd = new SqlCommand("INSERT INTO list_users(Nome,Apelido,Logg,Senha,Obs) VALUES(@nome,@apelido,@login,@senha,@obs)", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@nome", txtnome.Text);
+                    cmd.Parameters.AddWithValue("@apelido", txtapelido.Text);
+                    cmd.Parameters.AddWithValue("@login", txtlogin.Text);
+                    cmd.Parameters.AddWithValue("@senha", txtsenha.Text);
+                    cmd.Parameters.AddWithValue("@obs", txtobs.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Registro incluído com sucesso...");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro : " + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                    ExibirDados();
+                    LimparDados();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe todos os dados requeridos");
+            }
+        }
+
+       
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                txtnome.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtapelido.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtlogin.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtsenha.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                txtobs.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+                txtid.Text = Convert.ToString(ID);
+            }
+            catch
+            {
+                
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtnome.Text != "" && txtapelido.Text != "" && txtlogin.Text != "" && txtsenha.Text != "" && txtobs.Text != "")
+            {
+                try
+                { 
+                cmd = new SqlCommand("UPDATE list_users SET Nome=@nome, Apelido=@apelido, Logg=@login, Senha=@senha, Obs=@obs WHERE Id=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Parameters.AddWithValue("@nome", txtnome.Text);
+                cmd.Parameters.AddWithValue("@apelido", txtapelido.Text);
+                cmd.Parameters.AddWithValue("@login", txtlogin.Text);
+                cmd.Parameters.AddWithValue("@senha", txtsenha.Text);
+                cmd.Parameters.AddWithValue("@obs", txtobs.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registro atualizado com sucesso...");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro : " + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                    ExibirDados();
+                    LimparDados();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe todos os dados requeridos");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (ID != 0)
+            {
+                if (MessageBox.Show("Deseja Deletar este registro ?", "Agenda", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        cmd = new SqlCommand("DELETE list_users WHERE Id=@id", con);
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@id", ID);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("registro deletado com sucesso...!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro : " + ex.Message);
+                    }
+                    finally
+                    {
+                        con.Close();
+                        ExibirDados();
+                        LimparDados();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione um registro para deletar");
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+
+            if (ID != 0)
+
+            {
+                try
+                {
+                    con.Open();
+                    DataTable dt = new DataTable();
+                    cmd = new SqlCommand("SELECT * FROM list_users WHERE Id=@id", con);
+                    adapt.SelectCommand = cmd;
+                    cmd.Parameters.AddWithValue("@id", txtidpesquisa.Text);
+                    adapt.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+          
+        }
+    }
+
+}
