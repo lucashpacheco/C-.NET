372312d0f480918231b11e956dd6265dd5a04811
Lucas Pacheco <lucashpmendonca@gmail.com>
2020-02-01 19:00:41 -0300

    Atualização  - projeto atualmente pausado

--- LogSys/LogSys/principal.Designer.cs
+++ LogSys/LogSys/principal.Designer.cs
@@ -28,26 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(principal));
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.loggDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.senhaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listuserBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.logSysDataSet = new LogSys.LogSysDataSet();
+            this.list_userTableAdapter = new LogSys.LogSysDataSetTableAdapters.list_userTableAdapter();
+            this.pesquisaToolStrip = new System.Windows.Forms.ToolStrip();
+            this.pesquisaToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listuserBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logSysDataSet)).BeginInit();
+            this.pesquisaToolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 86);
+            this.label1.Location = new System.Drawing.Point(21, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Entrou";
+            this.label1.Text = "NOME";
             // 
-            // Form2
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pesquisar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Adicionar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(119, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Deletar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(213, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Atualizar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.loggDataGridViewTextBoxColumn,
+            this.senhaDataGridViewTextBoxColumn,
+            this.nomeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.listuserBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(336, 150);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // loggDataGridViewTextBoxColumn
+            // 
+            this.loggDataGridViewTextBoxColumn.DataPropertyName = "Logg";
+            this.loggDataGridViewTextBoxColumn.HeaderText = "Logg";
+            this.loggDataGridViewTextBoxColumn.Name = "loggDataGridViewTextBoxColumn";
+            // 
+            // senhaDataGridViewTextBoxColumn
+            // 
+            this.senhaDataGridViewTextBoxColumn.DataPropertyName = "Senha";
+            this.senhaDataGridViewTextBoxColumn.HeaderText = "Senha";
+            this.senhaDataGridViewTextBoxColumn.Name = "senhaDataGridViewTextBoxColumn";
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            // 
+            // listuserBindingSource
+            // 
+            this.listuserBindingSource.DataMember = "list_user";
+            this.listuserBindingSource.DataSource = this.logSysDataSet;
+            // 
+            // logSysDataSet
+            // 
+            this.logSysDataSet.DataSetName = "LogSysDataSet";
+            this.logSysDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // list_userTableAdapter
+            // 
+            this.list_userTableAdapter.ClearBeforeFill = true;
+            // 
+            // pesquisaToolStrip
+            // 
+            this.pesquisaToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pesquisaToolStripButton,
+            this.toolStripSeparator1,
+            this.toolStripTextBox1,
+            this.toolStripSeparator2,
+            this.toolStripButton1});
+            this.pesquisaToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.pesquisaToolStrip.Name = "pesquisaToolStrip";
+            this.pesquisaToolStrip.Size = new System.Drawing.Size(371, 25);
+            this.pesquisaToolStrip.TabIndex = 7;
+            this.pesquisaToolStrip.Text = "pesquisaToolStrip";
+            // 
+            // pesquisaToolStripButton
+            // 
+            this.pesquisaToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.pesquisaToolStripButton.Name = "pesquisaToolStripButton";
+            this.pesquisaToolStripButton.Size = new System.Drawing.Size(57, 22);
+            this.pesquisaToolStripButton.Text = "Pesquisa";
+            this.pesquisaToolStripButton.Click += new System.EventHandler(this.pesquisaToolStripButton_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 288);
+            this.ClientSize = new System.Drawing.Size(371, 288);
+            this.Controls.Add(this.pesquisaToolStrip);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Name = "Form2";
+            this.Name = "principal";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.principal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listuserBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logSysDataSet)).EndInit();
+            this.pesquisaToolStrip.ResumeLayout(false);
+            this.pesquisaToolStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +233,23 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private LogSysDataSet logSysDataSet;
+        private System.Windows.Forms.BindingSource listuserBindingSource;
+        private LogSysDataSetTableAdapters.list_userTableAdapter list_userTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn loggDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn senhaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ToolStrip pesquisaToolStrip;
+        private System.Windows.Forms.ToolStripButton pesquisaToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
     }
 }
--- LogSys/LogSys/principal.cs
+++ LogSys/LogSys/principal.cs
@@ -16,5 +16,63 @@ namespace LogSys
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void principal_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'logSysDataSet.list_user'. Você pode movê-la ou removê-la conforme necessário.
+            this.list_userTableAdapter.Fill(this.logSysDataSet.list_user);
+
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.list_userTableAdapter.FillBy(this.logSysDataSet.list_user);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pesquisaToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.list_userTableAdapter.Pesquisa(this.logSysDataSet.list_user);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
